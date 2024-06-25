6855fa3e021427eaaad51b3387839c0579ed63d5
yeaeun123 <163090433+yeaeun123@users.noreply.github.com>
2024-06-25 17:48:19 +0900

    Add files via upload

---
+++ Practice02_/src/com/practice02/Java2_12.java
@@ -0,0 +1,35 @@
+package com.practice02;
+
+import java.util.Scanner;
+
+public class Java2_12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner =new Scanner(System.in);
+		
+		System.out.println("숫자 3개를 입력해주세요.");
+		System.out.println("숫자1:");
+		int num1 = scanner.nextInt();
+		System.out.println("숫자2:");
+		int num2 = scanner.nextInt();
+		System.out.println("숫자3:");
+		int num3 = scanner.nextInt();
+	
+		int small = num1; //num1이 제일 작다고 가정했을떄
+		
+		if (num2 < small) { //num2가 small보다 작으면
+			small = num2;	//num2가 small로 지정됨
+		
+	}	if (num3 < small){
+			small = num3;
+	}	System.out.println("가장 작은 수는 "+small + "입니다.");
+
+	
+	
+		scanner.close();
+	}}
+		
+	
+
+
---
+++ Practice02_/src/com/practice02/Java2_17.java
@@ -0,0 +1,37 @@
+package com.practice02;
+
+import java.util.Scanner;
+
+public class Java2_17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("수익을 입력해주세요.");
+		System.out.print("금익:");
+		int money = scanner.nextInt(); // 금액 입력하는 곳 만들기
+		
+		double tax; //세금 변수 선언
+		if(money>0 && money <= 1000) {
+			tax = 0.09 * money; //1000이하인 경우 계산법 입력
+			System.out.println("소득세는"+ tax +"입니다.");
+		}else if (money>0 &&money <=4000) {
+			tax = 1000 * 0.09 + 0.18 * (money-1000);
+			System.out.println("소득세는"+ tax +"입니다.");
+		}else if (money >0 && money < 8000) {
+			tax = 1000*0.09 + 3000*0.18 + 0.27 * (money-4000);
+			System.out.println("소득세는"+ tax +"입니다.");
+		}else if (money>0&& money >= 8000){
+			tax = 1000*0.09 + 3000*0.18 + 4000*0.27 + 0.36 * (money-8000);
+			System.out.println("소득세는"+ tax +"입니다.");
+		}else{	
+			System.out.println("숫자를 잘못 입력하셨습니다.");
+		}
+		scanner.close();
+		}
+
+	}
+
+
